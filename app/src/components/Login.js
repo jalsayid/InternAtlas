2823e6244d1f60091999a744103fb03a16d1b223
Danah <dmea9009@gmail.com>
2025-05-03 20:23:15 +0300

    try

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -23,7 +23,7 @@ function Login({ onSwitchToRegister }) {
       setFormErrors(errors);
     } else {
       try {
-        const response = await fetch("http://localhost:3001/api/login", {
+        const response = await fetch("https://internatlas.onrender.com/api/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password }),
